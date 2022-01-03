11a735742fd00ebffa57594af9a40d0116d4333c
getcoden <337885169@qq.com>
2022-01-03 16:26:06 +0800

    baidu_AI 识别图片表格到 Excel

--- 10 有趣的脚本小程序/Download视频及youtube视频中仅下载音频.py
+++ 10 有趣的脚本小程序/Download视频及youtube视频中仅下载音频.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import youtube_dl
-
+"""
 # 仅下载视频
 times = int(input('输入要下载视频的数量:'))
 for i in range(times):
@@ -8,7 +8,7 @@ for i in range(times):
     ys_url = input(f'要下载{i+1}次的视频地址:')
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([ys_url])
-
+"""
 
 # 仅下载视频的音频
 
